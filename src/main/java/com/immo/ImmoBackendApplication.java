7f4fec8348ce39dde267fda1cdb4982dc3f955e5
thirtsoft <thirtsoft@gmail.com>
2021-08-16 16:06:57 +0000

    first commit

---
+++ src/main/java/com/immo/ImmoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.immo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImmoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImmoBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/immo/ImmoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.immo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImmoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
